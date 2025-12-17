3c3503d19f39aff56e8b291bec40ff05bfb2274d
Alex S. <alex@127.0.0.1>
2025-12-17 14:03:12 +0100

    find user by id

--- routes/route.js
+++ routes/route.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { getUsers, createUser, deleteUser, updateUser } from './../service/service.js';
+import { getUsers, createUser, deleteUser, updateUser, getUser } from './../service/service.js';
+
 
 const router = express.Router();
 
+
 router.get('/users', getUsers);
+router.get('/users/:user_id', getUser);
 router.post('/users', createUser);
 router.delete('/users/:user_id', deleteUser);
 router.put('/users/:user_id', updateUser);
--- service/service.js
+++ service/service.js
@@ -1,38 +1,46 @@
 import pool from './../db_config/db.js';
 
 export let getUsers = async function (req, res){
-    const users = await pool.getConnection()
+    let conn = pool.getConnection();
+    const users = await conn
         .then(conn => conn.query('SELECT * FROM users'))
-        .catch(err =>  err)
-
-    //console.log(users);
-    
+        .catch(err =>  err);
+    (await conn).release();
     res.json(users);
 }
 
 export let createUser = async function (req, res){
-    console.log(req);
-    
-    const query = await pool.getConnection()
+    let conn = pool.getConnection();
+    const query = await conn
         .then(conn => conn.query(`INSERT INTO users(user_name, email, password) VALUES("${req.body.user_name}", "${req.body.email}", "${req.body.password}")`))
         .catch(err => console.log(err));
+    (await conn).release();
     res.status(201).send({msg: "User angelegt"});
 }
 
 export let updateUser = async function (req, res) {
-    console.log(req.body);
-    
-    const user = await pool.getConnection()
+    let conn = pool.getConnection();
+    const user = await conn
         .then(conn => conn.query(`UPDATE users SET user_name = "${req.body.user_name}", email = "${req.body.email}" WHERE user_id = ${req.params.user_id}`))
-        .catch(err => err)
+        .catch(err => err);
+    (await conn).release();
     res.status(200).send({msg: "User updated"});
 }
 
 export let deleteUser = async function (req, res){
-    console.log(req.params.user_id);
-    
-    const user = await pool.getConnection()
+    let conn = pool.getConnection();
+    const user = await conn
         .then(conn => conn.query(`DELETE FROM users WHERE user_id = ${req.params.user_id}`))
         .catch(err => err);
+    (await conn).release();
     res.status(200).send({msg: "User gelöscht"});
+}
+
+export let getUser = async function (req, res){
+    let conn = pool.getConnection();
+    const user = await conn
+        .then(conn => conn.query(`SELECT user_id, user_name, email FROM users WHERE user_id = ${req.params.user_id}`))
+        .catch(err => err);
+    (await conn).release();
+    res.status(200).json(user);
 }
